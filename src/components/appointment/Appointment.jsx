4a836ece3f1fcc5225e57d7f06dd7ca4252e8fdb
Lehila <lehilasantillan2e39@gmail.com>
2022-04-21 03:07:09 -0500

    Si

---
+++ src/components/appointment/Appointment.jsx
@@ -0,0 +1,4 @@
+import i18next from '../../config/localization/i18n';
+import { useState, useEffect } from 'react';
+import Avatar from "../avatar";
+import { supabase } from '../../config/supabase';
---
+++ src/components/auth/Auth.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { supabase } from '../../supabaseClient';
+import React, { Component }  from 'react';
+
+export default function Auth() {
+  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      setLoading(true)
+      const { error } = await supabase.auth.signIn({ email })
+      if (error) throw error
+      alert('Check your email for the login link!')
+    } catch (error) {
+      alert(error.error_description || error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div className="row flex flex-center">
+      <div className="col-6 form-widget" aria-live="polite">
+        <h1 className="header">Intercuatrimestral - PWA    170735</h1>
+        <p className="description">Sign in via email</p>
+        {loading ? (
+          'Sending magic link...'
+        ) : (
+          <form onSubmit={handleLogin}>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              className="inputField"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button className="button block" aria-live="polite">
+              Verify
+            </button>
+          </form>
+        )}
+      </div>
+    </div>
+  )
+}
